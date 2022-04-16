6d7c231278f44474cf9ac8a8841e93ec23d5071b
witherpearl <83099794+witherpearl@users.noreply.github.com>
2022-04-16 18:15:39 -0500

    fix 4

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,7 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-		if (hasUpgrade('p', 11)) gain = gain.times(2)
+		if (hasUpgrade('M', 11)) gain = gain.times(2)
 	let gain = new Decimal(1)
 	return gain
 }

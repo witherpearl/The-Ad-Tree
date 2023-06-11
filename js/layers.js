cd3292e046054b5c33b2ec505e20a46a1ce63fcb
witherpearl <83099794+witherpearl@users.noreply.github.com>
2023-06-10 19:51:15 -0500

    Restarting

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "Pizzeria", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("s", {
+    name: "Spirit Orbs", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "tokens", // Name of prestige currency
-    baseResource: "dollars", // Name of resource prestige is based on
+    resource: "Spirit Orbs", // Name of prestige currency
+    baseResource: "Soul Essence", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -19,26 +19,26 @@ addLayer("p", {
     },
     upgrades: {
         11: {
-            title: "Branding",
+            title: "Self Enhance",
             description: "Double your point gain.",
-            cost: new Decimal(1),
+            cost: new Decimal(5),
         },
         12: {
-            title: "Ghost Kitchen",
-            description: "Increase dollars based on tokens.",
-            cost: new Decimal(5),
+            title: "Spirit Cycle",
+            description: "Focus your Soul Orbs to boost your Soul Essence.",
+            cost: new Decimal(10),
             effect() {
                 if (hasUpgrade('p',13)) {
-                   return player[this.layer].points.add(1).pow(0.55) 
+                   return player[this.layer].points.add(1).pow(0.60) 
                 }  else {
-                    return player[this.layer].points.add(1).pow(0.5)
+                    return player[this.layer].points.add(1).pow(0.30)
                 }
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
         13: {
-            title: "Better Cut",
-            description: "Get more from the ghost kitchen.",
+            title: "Tune Out",
+            description: "Get more from your Spirit Cycle.",
             cost: new Decimal(20),
         },
     },
@@ -47,22 +47,30 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for Tokens", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "s", description: "S: Reset for Spirit Orbs", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
+
+
+
+
+
+
+
+
 addLayer("f", {
     startData() { return {                  // startData is a function that returns default data for a layer. 
-        unlocked: true,                     // You can add more variables here to add them to your layer.
+        unlocked: false,                     // You can add more variables here to add them to your layer.
         points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
     }},
     color: "#4BDC13",                       // The color for this layer, which affects many elements.
-    resource: "locations",            // The name of this layer's main prestige resource.
+    resource: "Power Orbs",            // The name of this layer's main prestige resource.
     row: 1,                                 // The row this layer is on (0 is the first row).
-    baseResource: "points",                 // The name of the resource your prestige gain is based on.
+    baseResource: "Spirit Orbs",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
 
-    requires: new Decimal(1000),              // The amount of the base needed to  gain 1 of the prestige currency.
+    requires: player.points(1000),              // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
 
     type: "static",                         // Determines the formula used for calculating prestige currency.
@@ -75,7 +83,7 @@ addLayer("f", {
         return new Decimal(1)
     },
     layerShown() {
-        return true
+        return false
     },         // Returns a bool for if this layer's node should be visible in the tree.
 
     upgrades: {
--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The Fnaf Tree",
+	name: "The Spirit Tree",
 	id: "mymod",
 	author: "witherpearl",
-	pointsName: "dollars",
+	pointsName: "Soul Essence",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -14,13 +14,13 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.1",
-	name: "Literally nothing",
+	name: "Base two nodes",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
+	<h3>v0.1</h3><br>
 		- Added things.<br>
-		- Added stuff.`
+		- Changed stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
